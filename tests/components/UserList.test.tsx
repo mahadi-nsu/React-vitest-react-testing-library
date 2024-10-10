23c7d0e49854013d82dffe495d2d5f5c981f558b
mahadi-nsu <mahadi.hassan@northsouth.edu>
2024-10-10 22:48:05 +0600

    feat : more test cases added for link navigation

--- tests/components/UserList.test.tsx
+++ tests/components/UserList.test.tsx
@@ -1,36 +1,43 @@
-import { render, screen } from '@testing-library/react';
-import UserList from '../../src/components/UserList';
-import { User } from '../../src/entities';
+import { render, screen } from "@testing-library/react";
+import UserList from "../../src/components/UserList";
+import { User } from "../../src/entities";
 
 describe("UserList", () => {
-    const usersWithData : User[] = [
-        {
-            id: 1,
-            name: "John",
-            isAdmin: true,
-        },
-        {
-            id: 2,
-            name: "Jane",
-            isAdmin: false,
-        },
-    ];
+  const usersWithData: User[] = [
+    {
+      id: 1,
+      name: "John",
+      isAdmin: true,
+    },
+    {
+      id: 2,
+      name: "Jane",
+      isAdmin: false,
+    },
+  ];
 
-    const usersWithoutData : User[] = [];
+  const usersWithoutData: User[] = [];
 
-    it("should render list of users", () => {
-        render(<UserList users={usersWithData} />);
-        const list = screen.getByRole("list");
-        expect(list).toBeInTheDocument();
-        expect(list.children.length).toBe(usersWithData.length);
+  it("should render list of users", () => {
+    render(<UserList users={usersWithData} />);
+    const list = screen.getByRole("list");
+    expect(list).toBeInTheDocument();
+    expect(list.children.length).toBe(usersWithData.length);
+  });
+
+  it("should render no users available message", () => {
+    render(<UserList users={usersWithoutData} />);
+    const message = screen.getByText(/no users available/i);
+    expect(message).toBeInTheDocument();
+  });
+
+  it("should naviagte to users/${user.id}", () => {
+    render(<UserList users={usersWithData} />);
+    usersWithData.forEach((user) => {
+      const link = screen.getByRole("link", { name: user.name });
+      console.log("link", link);
+      expect(link).toBeInTheDocument();
+      expect(link).toHaveAttribute("href", `/users/${user.id}`);
     });
-
-
-    it("should render no users available message", () => {
-        render(<UserList users={usersWithoutData} />);
-        const message = screen.getByText(/no users available/i);
-        expect(message).toBeInTheDocument();
-    });
-    
-
-})
+  });
+});
