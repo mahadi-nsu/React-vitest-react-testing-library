5775bea7cfc1100eea6eadc3ce8efedeb8006ae3
mahadi-nsu <mahadi.hassan@northsouth.edu>
2024-10-20 10:54:19 +0600

    feat : checkbox interation test cases added in terms and conditions component

---
+++ tests/components/TermsAndConditions.test.tsx
@@ -0,0 +1,40 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import TermsAndConditions from "../../src/components/TermsAndConditions";
+
+describe("TermsAndConditions", () => {
+  it("should render terms and conditions", () => {
+    render(<TermsAndConditions />);
+    const heading = screen.getByRole("heading");
+    expect(heading).toBeInTheDocument();
+    // console.log("heading", heading);
+    expect(heading).toHaveTextContent(/terms & conditions/i);
+  });
+
+  it("should render checkbox", () => {
+    render(<TermsAndConditions />);
+    const checkbox = screen.getByRole("checkbox");
+    console.log("checkbox", checkbox);
+    expect(checkbox).toBeInTheDocument();
+    expect(checkbox).not.toBeChecked();
+  });
+
+  it("should render submit button", () => {
+    render(<TermsAndConditions />);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toBeDisabled();
+  });
+
+  it("should enable the submit button when checkbox is checked", () => {
+    render(<TermsAndConditions />);
+    const checkbox = screen.getByRole("checkbox");
+    const button = screen.getByRole("button");
+    // checkbox.click();
+    // expect(checkbox).toBeChecked();
+    // expect(button).not.toBeDisabled();
+
+    fireEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+    expect(button).not.toBeDisabled();
+  });
+});
